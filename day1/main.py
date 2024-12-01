e992af77a0ca7654ca451e97d2b4415c1977916b
bili <91601637+biiiliii@users.noreply.github.com>
2024-12-01 18:25:18 +0100

    Added day 2 second part

--- day1/main.py
+++ day1/main.py
@@ -1,7 +1,15 @@
 with open("input.txt") as f:
     leftcol, rightcol = zip(*[map(int, line.split()) for line in f])
 
-leftcol = sorted(leftcol)
-rightcol = sorted(rightcol)
-result = sum(abs(l - r) for l, r in zip(leftcol, rightcol))
-print(result)
+def p1():
+    leftcol = sorted(leftcol)
+    rightcol = sorted(rightcol)
+    result = sum(abs(l - r) for l, r in zip(leftcol, rightcol))
+    print(result)
+
+def p2():
+    result = sum(n * rightcol.count(n) for n in leftcol)
+    print(result)
+
+p1()
+p2()
