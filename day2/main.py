1112eded5b4e4a43192cabddf66e1c66f3c02884
bili <91601637+biiiliii@users.noreply.github.com>
2024-12-02 10:15:50 +0100

    p2 solved

--- day2/main.py
+++ day2/main.py
@@ -5,8 +5,32 @@ def p1():
     for lst in data:
         if sorted(lst) == lst or sorted(lst, reverse=True) == lst:
             if all(1 <= abs(a - b) <= 3 for a, b in zip(lst, lst[1:])):
+                safe_count += 1 
+    print(safe_count)
+
+def p2():
+    safe_count = 0
+    for lst in data:
+        if sorted(lst) == lst or sorted(lst, reverse=True) == lst:
+            if all(1 <= abs(a - b) <= 3 for a, b in zip(lst, lst[1:])):
                 safe_count += 1
+            else:
+                for i in range(len(lst)):
+                    new_lst = lst.copy()
+                    del new_lst[i]
+                    if sorted(new_lst) == new_lst or sorted(new_lst, reverse=True) == new_lst:
+                        if all(1 <= abs(a - b) <= 3 for a, b in zip(new_lst, new_lst[1:])):
+                            safe_count += 1
+                            break
+        else:
+            for i in range(len(lst)):
+                new_lst = lst.copy()
+                del new_lst[i]
+                if sorted(new_lst) == new_lst or sorted(new_lst, reverse=True) == new_lst:
+                    if all(1 <= abs(a - b) <= 3 for a, b in zip(new_lst, new_lst[1:])):
+                        safe_count += 1
+                        break
     print(safe_count)
 
 
-p1()
+p2()
