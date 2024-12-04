7e0360abfcfefef627dfd7609c29ed23391edcf9
bili <91601637+biiiliii@users.noreply.github.com>
2024-12-04 10:36:04 +0100

    Added p2

--- day4/main.py
+++ day4/main.py
@@ -24,11 +24,23 @@ def p1():
             diagonal = "".join([data[i - k][j + k] for k in range(4)])
             comptador += diagonal.count("XMAS")
             comptador += diagonal[::-1].count("XMAS") 
-    
-    
     print(comptador)
 
+def p2():
+    global data
 
+    f, c = len(data), len(data[0])
+    comptador = 0
 
+    for i in range(1, f - 1):
+        for j in range(1, c - 1):
+            if data[i][j] == 'A':
+                diagonal_right = "".join([data[i + k][j + k] for k in range(-1, 2)])
+                diagonal_left = "".join([data[i - k][j + k] for k in range(-1, 2)])
+                if (diagonal_left.count("MAS") + diagonal_left[::-1].count("MAS")) and (diagonal_right.count("MAS") + diagonal_right[::-1].count("MAS")):
+                    comptador += 1
+
+    print(comptador)
 
 p1()
+p2()
