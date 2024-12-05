42a0819957a2b7a1e5d47e18cb22216f8b95a1ab
biiiliii <bielinyu@gmail.com>
2024-12-05 19:13:58 +0100

    ruby is now shorter

--- day1/main.rb
+++ day1/main.rb
@@ -1,8 +1,8 @@
 puts File.readlines("a").map{
-|l|l.split.map(&:to_i)
+_1.split.map(&:to_i)
 }.transpose.then{
 |l,r|[l.sort.zip(r.sort).sum{
-|a,b|(a-b).abs
+(_1-_2).abs
 },l.sum{
-|n|n*r.count(n)
+_1*r.count(_1)
 }]}
