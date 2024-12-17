abbbeefebf3c9d63606f7b1e11f7dc97d5547e06
bili <91601637+biiiliii@users.noreply.github.com>
2024-12-17 11:54:35 +0100

    day17 p2 not solved yet

--- day17/main.py
+++ day17/main.py
@@ -44,4 +44,39 @@ class solution(SolutionBase):
             instruction_pointer += 2
         
         print(f"Register A: {rA}, Register B: {rB}, Register C: {rC}")
-        return ",".join(value) 
+        return ",".join(value)
+    
+    def find(self, program, ans):
+        if program == []: return ans
+        for t in range(8):
+            a = ans << 3 | t
+            b = a % 8
+            b = b ^ 2
+            c = a >> b
+            b = b ^ 7
+            b = b ^ c
+            a = a >> 3
+            if b % 8 == program[-1]:
+                sub = self.find(program[:-1], a)
+                if sub != None: continue
+                return sub
+
+    def p2(self):
+        program = list(map(int, re.findall(r"\d+", self.data)[3:]))
+
+        
+        """
+        Program: 2,4,1,2,7,5,1,7,4,4,0,3,5,5,3,0
+
+
+        b = a % 8 --> # 2,4
+        b = b ^ 2  --> # 1,2
+        c = a >> b --> # 7,5
+        b = b ^ 7 --> # 1,7
+        b = b ^ c --> # 4,4
+        a = a >> 3 --> # 0,3
+        # output b % 8 --> # 5,5
+        if a != 0: pointer = 0 --> # 3,0
+        """
+
+        return self.find(program, 0)
