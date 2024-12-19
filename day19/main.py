85c909c316de16e18555303a63e8bcd070f67fe4
bili <91601637+biiiliii@users.noreply.github.com>
2024-12-19 10:08:23 +0100

    d19 p2

--- day19/main.py
+++ day19/main.py
@@ -1,4 +1,5 @@
 from utils.solutionBase import SolutionBase
+from functools import cache
 
 class solution(SolutionBase):
     patterns, desings = open("day19/input.txt").read().split("\n\n")
@@ -17,9 +18,19 @@ class solution(SolutionBase):
         p = [t.strip() for t in self.patterns.split(",")]
         d = [line for line in self.desings.split("\n")]
 
-
+        return sum(1 if self.possible(p, design) else 0 for design in d)
+    
+    @cache
+    def possibilities(self, design, patterns):
+        if design == "": return 1
         c = 0
-        for i in range(len(d)):
-            if self.possible(p, d[i]):
-                c += 1
+        for pattern in patterns:
+            if design.startswith(pattern):
+                c += self.possibilities(design[len(pattern):], patterns)
         return c
+
+    def p2(self):
+        p = tuple(t.strip() for t in self.patterns.split(","))
+        d = [line for line in self.desings.split("\n")]
+        
+        return sum(self.possibilities(design, p) for design in d)
